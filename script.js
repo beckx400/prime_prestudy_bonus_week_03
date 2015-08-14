df8434cf6e8ca9df2d5a7fd6f110a2d5fd41e74d
David Beck <beckx400@d.umn.edu>
2015-08-14 13:15:06 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function first (value){
-	return value *= 10;
+	return value * 10;
 }
 function second(value){
 	if (value > 40){
-		return value /= 10;
+		return value / 10;
 	} else {
-		return value +=15;
+		return value + 15;
 	}
 }
 function third(value){
-	return value -= 11;
+	return value - 11;
 }
 
 var x = first(second(third(5)));
